c4af9042d4245490c6de2566a4a187c10a99d273
elnata <elnatan.des@gmail.com>
2016-02-17 15:14:21 -0300

    RG

---
+++ index.php
@@ -0,0 +1,402 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Revelando Gigantes 2016.1 - UniSigma</title>
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/freelancer.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+	<link rel="shortcut icon" href="http://unisigma.com.br/Site/images/favicon.png">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="revelando" href="#page-top"><h3>Revelando Gigantes 2016.1</h3></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#Inscricao">Inscrição</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#sobre">Sobre</a>
+                    </li>
+
+                     <li role="presentation" class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      Downloads <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+       <li><a href="downloads/Edital.pdf"target="_blank">Edital</a></li>
+       <li><a href="downloads/modelo curriculo UniSigma 2015.2.docx"target="_blank">Modelo de CV</a></li>
+    </ul>
+  </li>
+                   
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img class="img-responsive img-circle" src="img/profile2.jpg" height="180" width="320" alt="">
+                    <div class="intro-text">
+                        <span class="name">UniSigma</span>
+                        <hr class="star-light">
+                        <span class="skills">Revelando Gigantes 2016.1</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Pagina de inscrição -->
+    <section id="Inscricao">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Inscrição</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+        
+
+    <!--inicio do formulario de inscrição-->
+         <form class="form-horizontal inscricao">
+<fieldset>
+
+<!-- Ftexto do form -->
+<h4><i><b>"</b>Este processo seletivo tem como objetivo geral selecionar e capacitar alunos para compor o quadro de membros da empresa na função de trainees. Os membros terão a oportunidade de vivenciar o dia a dia da empresa, podendo identificar as áreas nas quais gostariam de atuar."</i></h4>
+
+<br>
+<br>
+
+
+
+<!-- Text input do nome-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Nome">Nome completo</label>  
+  <div class="col-md-8">
+  <input id="Nome" name="Nome" placeholder="Nome" class="form-control input-md" required="" type="text">
+    
+  </div>
+</div>
+
+<!-- Text input da data de nascimento-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Data">Data de Nascimento:</label>  
+  <div class="col-md-8">
+  <input id="Data" name="Data" placeholder="Exemplo: 16/03/1996" class="form-control input-md" type="text">
+    
+  </div>
+</div>
+
+<!-- Text input do curso-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Curso">Curso:</label>  
+  <div class="col-md-8">
+  <input id="Curso" name="Curso" placeholder="Curso" class="form-control input-md" required="" type="text">
+    
+	  </div>
+</div>
+
+<!-- Text input do email-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Curso">Email:</label>  
+  <div class="col-md-8">
+  <input id="email" name="email" placeholder="Email" class="form-control input-md" required="" type="text">
+    
+  </div>
+</div>
+
+
+<!-- Text input do telefone-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Curso">Telefone(preferência whatsapp:</label>  
+  <div class="col-md-8">
+  <input id="telefone" name="telefone" placeholder="Ex.: (83) 91234-5678 " class="form-control input-md" required="" type="text">
+    
+  </div>
+</div>
+
+
+<!-- Text input do facebook-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Curso">Facebook:</label>  
+  <div class="col-md-8">
+  <input id="facebook" name="facebook" placeholder="https://www.facebook.com/unisigma" class="form-control input-md" required="" type="text">
+    
+  </div>
+</div>
+
+
+<!-- Text input Necessidades Especiais-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Curso">Necessidades Especiais?</label>  
+  <div class="col-md-8">
+  <input id="Necessidades" name="Necessidades" placeholder="Se sim, qual?" class="form-control input-md" required="" type="text">
+    
+  </div>
+</div>
+
+
+
+
+<!--Text input periodo -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="Período">Período:</label>
+  <div class="col-md-4"> 
+    <label class="radio-inline" for="Período-0">
+      <input name="Período" id="Período-0" value="1º" checked="checked" type="radio">
+      1
+    </label> 
+    <label class="radio-inline" for="Período-1">
+      <input name="Período" id="Período-1" value="2º" type="radio">
+      2
+    </label> 
+    <label class="radio-inline" for="Período-2">
+      <input name="Período" id="Período-2" value="3º" type="radio">
+      3
+    </label> 
+    <label class="radio-inline" for="Período-3">
+      <input name="Período" id="Período-3" value="4º" type="radio">
+      4
+    </label> 
+    <label class="radio-inline" for="Período-4">
+      <input name="Período" id="Período-4" value="5º" type="radio">
+      5
+    </label> 
+    <label class="radio-inline" for="Período-5">
+      <input name="Período" id="Período-5" value="6º" type="radio">
+      6
+    </label> 
+    <label class="radio-inline" for="Período-6">
+      <input name="Período" id="Período-6" value="7º" type="radio">
+      7
+    </label> 
+    <label class="radio-inline" for="Período-7">
+      <input name="Período" id="Período-7" value="8º" type="radio">
+      8
+    </label>
+  </div>
+</div>
+
+
+
+<!-- Text input experiência profissional -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="experiencia">Possui experiência profissional?</label>
+  <div class="col-md-4">
+  <div class="checkbox">
+    <label for="experiencia-0">
+      <input name="experiencia" id="experiencia-0" value="sim" type="checkbox">
+      Sim
+    </label>
+	</div>
+  <div class="checkbox">
+    <label for="experiencia-1">
+      <input name="experiencia" id="experiencia-1" value="nao" type="checkbox">
+      Não
+    </label>
+	</div>
+  </div>
+</div>
+
+<!--Text input Motivação -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="textarea">Qual a sua motivação para ser da UniSigma?</label>
+  <div class="col-md-4">                     
+    <textarea class="form-control" id="textarea" name="textarea"></textarea>
+  </div>
+</div>
+
+
+<!--text input dos Cargos-->
+<br>
+<br>
+<div class="opcao">
+<h3 class="" for="experiencia">Área e Cargo de Interesse</h3>
+
+<br>
+
+
+<div class="col-md-8"><label class="title">Diretoria de Gestão de Pessoas</label><div class="large"><span><select name="select" class="form-control input-md cargo" >
+		<option value="option 1">Não tenho interesse</option>
+		<option value="option 2">Assessor de Captação e Retenção de Talentos</option>
+		<option value="option 3">Assessor de Desenvolvimento</option></select></span></div></div>
+
+
+<div class="col-md-8"><label class="title">Diretoria de Qualidade</label><div class="large"><span><select name="select" class="form-control input-md cargo" >
+
+		<option value="option 1">Não tenho interesse</option>
+		<option value="option 2">Assessor de Qualidade</option>
+		</select></span></div></div>
+
+
+
+<div class="col-md-8"><label class="title">Diretoria Administrativo Financeira</label><div class="large"><span><select name="select" class="form-control input-md cargo" >
+
+		<option value="option 1">Não tenho interesse</option>
+		<option value="option 2">Assessor Administrativo Financeiro</option>
+		</select></span></div></div>
+
+
+
+<div class="col-md-8"><label class="title">Diretoria de Marketing</label><div class="large"><span><select name="select" class="form-control input-md cargo" >
+
+		<option value="option 1">Não tenho interesse</option>
+		<option value="option 2">Assessor de Mídia e Comunicação</option>
+		<option value="option 3">Assessor Comercial</option>
+		<option value="option 4">Assessor de Relacionamentos</option>
+		</select></span></div></div>
+
+
+
+<div class="col-md-8"><label class="title">Diretoria de Projetos</label><div class="large"><span><select name="select" class="form-control input-md cargo" >
+
+		<option value="option 1">Não tenho interesse</option>
+		<option value="option 2">Desenvolvedor</option>
+		<option value="option 2">Consultor de Geoprocessamento</option>
+		<option value="option 2">Consultor de Administração</option>
+		</select></span></div></div>
+
+
+
+
+</div>
+<!--fim do input dos cargos-->
+
+
+<!--botão enviar-->
+
+<div class="enviar">
+<input type="submit" class="btn btn-info" value="Enviar">
+</div>
+<!--fim do botão-->
+
+
+
+
+</fieldset>
+</form>
+<!--fim do formulario de inscrição-->
+
+
+
+
+
+
+        </div>
+    </section>
+
+    <!-- Sobre -->
+    <section class="success" id="sobre">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Sobre</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+            <div>
+                <div>
+                <h3 class="text-center">UniSigma</h3>
+                    <p>A UniSigma nasceu em 2010, por meio da atuação de alguns jovens estudantes que almejavam aplicar seus conhecimentos aprendidos no curso em um ambiente com liberdade e espaço empreendedor. 
+                    Com o apoio do Instituto Federal da Paraíba, da Federação e de outras EJs a UniSigma podê nascer e crescer, englobando os cursos de Administração, Sistemas para Internet e Geoprocessamento. 
+                    Desenvolvemos soluções para mercado em nossas três áreas de atuação,
+                     focando na qualidade dos serviços e buscando desafios continuamente.</p>
+                </div>
+                <div>
+                <h3 class="text-center">Revelando Gigantes</h3>
+                    <p>Nosso processo seletivo ocorre semestralmente. Buscamos jovens que não temem desafios e que tenham o desejo de fazer a diferença. Você é um deles?</p>
+                </div>
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <a href="http://unisigma.com.br/" class="btn btn-lg btn-outline">
+                        <i class="fa "></i> Mais sobre a UniSigma
+
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        Copyright &copy; <a href="http://unisigma.com.br/">UniSigma</a> <?php echo date('Y') //pega o ano ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/freelancer.js"></script>
+
+</body>
+
+</html>
